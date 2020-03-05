3488874cd876ba9c6b12da47ee8703f6d9b85ef4
Juan Fernandez-Corugedo Igual <jfcorugedo@gmail.com>
2020-03-05 14:55:32 +0100

    Fixes resource

--- src/main/java/micronautfw/demo/starwars/PeopleResource.java
+++ src/main/java/micronautfw/demo/starwars/PeopleResource.java
@@ -7,15 +7,11 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
-import io.reactivex.Flowable;
 import io.reactivex.Maybe;
-import io.reactivex.Single;
 import micronautfw.demo.starwars.dto.Person;
 
 import javax.inject.Inject;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
 
 @Controller(value = "starwars/people", produces = MediaType.APPLICATION_JSON)
 public class PeopleResource {
@@ -29,16 +25,6 @@ public class PeopleResource {
 
         return httpClient
             .retrieve(HttpRequest.GET("/people/" + id), Person.class)
-            .firstElement()
-            .flatMap(person -> {
-                List<Single<String>> films = person.getFilms().stream().map(filmUrl -> getFilmTitle(filmUrl)).collect(Collectors.toList());
-                films.get(0).
-            });
-    }
-
-    private Single<String> getFilmTitle(String filmUrl) {
-        Maybe<Map> film = httpClient.retrieve(HttpRequest.GET(filmUrl), Map.class).firstElement();
-
-        return film.map(filmData -> (String)filmData.get("title")).toSingle("ERROR");
+            .firstElement();
     }
 }
